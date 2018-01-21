5251edc7d816078bd2d3b7c382bb30b24dacff7e
Archawin <zepalzth@gmail.com>
2018-01-21 14:38:19 +0700

    Fix source

--- zsh/install.sh
+++ zsh/install.sh
@@ -4,10 +4,10 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 cat ~/dotfiles/zsh/themes/cobalt2.zsh-theme > ~/.oh-my-zsh/themes/cobalt2.zsh-theme
 sed -i 's/ZSH_THEME="agnoster"/ZSH_THEgME="cobalt2"/g' ~/.zshrc
 
-//Install powerline font
-# git clone https://github.com/powerline/fonts.git --depth=1
-# cd fonts
-# ./install.sh
-# cd ..
-# rm -rf fonts
+# Install powerline font
+git clone https://github.com/powerline/fonts.git --depth=1
+cd fonts
+./install.sh
+cd ..
+rm -rf fonts
 echoGreen "*** oh-my-zsh is ready ***"
