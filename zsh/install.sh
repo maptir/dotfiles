1768d550c34621034a157cc72fb07a893a735e7e
Archawin <zepalzth@gmail.com>
2018-01-21 16:01:51 +0700

    Fix zsh install

--- zsh/install.sh
+++ zsh/install.sh
@@ -2,9 +2,10 @@ echoGreen "*** Installing oh-my-zsh ***"
 install zsh
 0>/dev/null sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 cat ~/dotfiles/zsh/themes/cobalt2.zsh-theme > ~/.oh-my-zsh/themes/cobalt2.zsh-theme
-sed -i 's/ZSH_THEME="agnoster"/ZSH_THEME="cobalt2"/g' ~/.zshrc
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="cobalt2"/g' ~/.zshrc
 
 # Install powerline font
+
 git clone https://github.com/powerline/fonts.git --depth=1
 cd fonts
 ./install.sh
