791e7e4c8a8ea365021cd0a352858db66ccbb5ee
Archawin <zepalzth@gmail.com>
2018-01-22 23:55:14 +0700

    Edit starter

--- starter.sh
+++ starter.sh
@@ -14,7 +14,7 @@ fi
 rm -rf ~/dotfiles
 git clone https://github.com/zepalz/dotfiles.git ~/dotfiles
 
-source ~/dotfiles/source.sh;
+bash -c "source ~/dotfiles/source.sh"
 echoRed "Start installing ..."
 echoGreen "*** Updating apt ***"
 sudo apt update
