cd6bad80db5f948eaf0afc3e5aae7a2477fa63e0
Archawin <zepalzth@gmail.com>
2018-01-23 00:02:55 +0700

    Fix guake bug

--- guake/install.sh
+++ guake/install.sh
@@ -2,7 +2,7 @@ echoGreen "*** Installing Guake ***"
 install guake
 echoWhite "Do you want guake to autostart [y/n]"
 read choose
-if [ choose == 'y']; then
+if [ choose == 'y' ]; then
   cat ~/dotfiles/guake/autostart/guake.desktop > ~/.config/autostart/guake.desktop
 fi
 echoGreen "*** Guake is ready ***"
