bbaeea05bdc3b1afc9daed3d49f565f85b6daea7
Archawin Tirugsapun <zepalzth@gmail.com>
2019-04-25 23:39:13 +0700

    Update install.sh

--- applications/install.sh
+++ applications/install.sh
@@ -5,11 +5,16 @@ install default-jdk
 install default-jre
 
 sudo snap install eclipse --classic
-sudo snap install ubuntu-make --classic
 sudo snap install node --channel=10/stable --classic
 sudo snap install postman
 sudo snap install spotify
 sudo snap install discord
 sudo snap install vlc
 sudo snap install telegram-desktop
+sudo snap install docker
+sudo snap install kubectl --classic
+sudo snap install slack --classic
+sudo snap install chromium
+sudo snap install go --classic
+sudo snap install robo3t-snap
 echoGreen "*** All applications is ready ***"
